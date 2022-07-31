f0d4f6f87417721a93d3a87bf9d59b2f4fe8cf06
batuogkoc <bgunduz21@ku.edu.tr>
2022-07-31 22:59:27 +0300

    have the infamous teapot now the new half-vectorised implementation gets 1.2 FPS old one gets 0.5 old one with fillPoly gets 0.7

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 from scipy.spatial.transform import Rotation as R
 from collections import deque 
 import math as m
+from stl import mesh
 WIDTH = 1600
 HEIGHT = 900
 FPS = 30
@@ -50,7 +51,9 @@ def smooth_triangle_lighting(image, triangle, colors):
     yv_m = yv[mask]
 
     xv_m[xv_m>=WIDTH] = WIDTH-1
+    xv_m[xv_m<0] = 0
     yv_m[yv_m>=HEIGHT] = HEIGHT-1
+    yv_m[yv_m<0] = 0
     
     def mul(a, b) :
         # Multiply two vectors into a matrix
@@ -61,7 +64,7 @@ def smooth_triangle_lighting(image, triangle, colors):
     image[yv_m, xv_m] = colors
     return image
 
-def render_triangles(image, triangles, triangle_colors, camera_position, camera_rotation, light_position):
+def render_triangles(image, triangles, triangle_colors, camera_position, camera_rotation, light_position, global_lighting=False):
     # vertex_1s = triangles[:,0]
     # vertex_2s = triangles[:,1]
     # vertex_3s = triangles[:,2]
@@ -107,50 +110,58 @@ def render_triangles(image, triangles, triangle_colors, camera_position, camera_
     # triangles[:,:,0] += WIDTH//2
     # triangles[:,:,1] += HEIGHT//2
     
-    # corner_colors = np.stack(((triangle_colors.T*v1s_brightness).T, (triangle_colors.T*v2s_brightness).T, (triangle_colors.T*v3s_brightness).T), axis=1)
+    # if global_lighting:
+    #     corner_colors = np.stack((triangle_colors, triangle_colors, triangle_colors), axis=1)
+    # else:
+    #     corner_colors = np.stack(((triangle_colors.T*v1s_brightness).T, (triangle_colors.T*v2s_brightness).T, (triangle_colors.T*v3s_brightness).T), axis=1)
     # for colors, triangle in zip(corner_colors, triangles):
     #     smooth_triangle_lighting(image, triangle.reshape((3,2)).astype(int), (colors*15).astype(np.uint8))
-    for triangle, color in zip(triangles, triangle_colors):
-        vertex_1, vertex_2, vertex_3 = triangle.reshape(-1,3)
-        vertex_1 = vertex_1.reshape(3,1)
-        vertex_2 = vertex_2.reshape(3,1)
-        vertex_3 = vertex_3.reshape(3,1)
-        center = (vertex_1 + vertex_2 + vertex_3)/3
-        edge_1 = vertex_1-vertex_3
-        edge_2 = vertex_2-vertex_3
-        normal = np.cross(edge_1, edge_2, axis=0)
-        normal_len = np.linalg.norm(normal)
-        v1_distance_to_light = (vertex_1-light_position)
-        v2_distance_to_light = (vertex_2-light_position)
-        v3_distance_to_light = (vertex_3-light_position)
+    
+    # for triangle, color in zip(triangles, triangle_colors):
+    #     vertex_1, vertex_2, vertex_3 = triangle.reshape(-1,3)
+    #     vertex_1 = vertex_1.reshape(3,1)
+    #     vertex_2 = vertex_2.reshape(3,1)
+    #     vertex_3 = vertex_3.reshape(3,1)
+    #     center = (vertex_1 + vertex_2 + vertex_3)/3
+    #     edge_1 = vertex_1-vertex_3
+    #     edge_2 = vertex_2-vertex_3
+    #     normal = np.cross(edge_1, edge_2, axis=0)
+    #     normal_len = np.linalg.norm(normal)
+    #     v1_distance_to_light = (vertex_1-light_position)
+    #     v2_distance_to_light = (vertex_2-light_position)
+    #     v3_distance_to_light = (vertex_3-light_position)
 
-        v1_distance_to_light_norm = np.linalg.norm(v1_distance_to_light)
-        v2_distance_to_light_norm = np.linalg.norm(v2_distance_to_light)
-        v3_distance_to_light_norm = np.linalg.norm(v3_distance_to_light)
+    #     v1_distance_to_light_norm = np.linalg.norm(v1_distance_to_light)
+    #     v2_distance_to_light_norm = np.linalg.norm(v2_distance_to_light)
+    #     v3_distance_to_light_norm = np.linalg.norm(v3_distance_to_light)
         
-        cos_angle_v1 = -(normal.T @ v1_distance_to_light)[0,0]/(normal_len*v1_distance_to_light_norm)
-        cos_angle_v2 = -(normal.T @ v2_distance_to_light)[0,0]/(normal_len*v2_distance_to_light_norm)
-        cos_angle_v3 = -(normal.T @ v3_distance_to_light)[0,0]/(normal_len*v3_distance_to_light_norm)
-        v1_brightness = max(0, cos_angle_v1*(1/v1_distance_to_light_norm**2)) 
-        v2_brightness = max(0, cos_angle_v2*(1/v2_distance_to_light_norm**2)) 
-        v3_brightness = max(0, cos_angle_v3*(1/v3_distance_to_light_norm**2)) 
-        distance_to_camera = (center-camera_position)
-        cos_angle_camera = -(normal.T @ distance_to_camera)
-        triangle.reshape(-1,3,3)
-        if cos_angle_camera > 0:
-            triangle = (np.linalg.inv(camera_rotation) @ (triangle.reshape(-1,3).T - camera_position)).T.reshape(-1,3,3)
-            triangle[:,:,0] /= triangle[:,:,2]
-            triangle[:,:,1] /= triangle[:,:,2]
+    #     cos_angle_v1 = -(normal.T @ v1_distance_to_light)[0,0]/(normal_len*v1_distance_to_light_norm)
+    #     cos_angle_v2 = -(normal.T @ v2_distance_to_light)[0,0]/(normal_len*v2_distance_to_light_norm)
+    #     cos_angle_v3 = -(normal.T @ v3_distance_to_light)[0,0]/(normal_len*v3_distance_to_light_norm)
+    #     v1_brightness = max(0, cos_angle_v1*(1/v1_distance_to_light_norm**2)) 
+    #     v2_brightness = max(0, cos_angle_v2*(1/v2_distance_to_light_norm**2)) 
+    #     v3_brightness = max(0, cos_angle_v3*(1/v3_distance_to_light_norm**2)) 
+    #     distance_to_camera = (center-camera_position)
+    #     cos_angle_camera = -(normal.T @ distance_to_camera)
+    #     triangle.reshape(-1,3,3)
+    #     if cos_angle_camera > 0:
+    #         triangle = (np.linalg.inv(camera_rotation) @ (triangle.reshape(-1,3).T - camera_position)).T.reshape(-1,3,3)
+    #         triangle[:,:,0] /= triangle[:,:,2]
+    #         triangle[:,:,1] /= triangle[:,:,2]
             
-            triangle = triangle[:,:,0:2]
-            triangle *= HEIGHT//2
-            triangle[:,:,0] += WIDTH//2
-            triangle[:,:,1] += HEIGHT//2
+    #         triangle = triangle[:,:,0:2]
+    #         triangle *= HEIGHT//2
+    #         triangle[:,:,0] += WIDTH//2
+    #         triangle[:,:,1] += HEIGHT//2
 
-            colors = np.array([color*v1_brightness, color*v2_brightness, color*v3_brightness])
+    #         if global_lighting:
+    #             colors = np.array([color, color, color])
+    #         else:
+    #             colors = np.array([color*v1_brightness, color*v2_brightness, color*v3_brightness])
 
-            smooth_triangle_lighting(image, triangle.reshape((3,2)).astype(int), (colors*15).astype(np.uint8))
-            # image = cv2.fillPoly(image, (triangle.astype(np.int),), (255,255,255))
+    #         smooth_triangle_lighting(image, triangle.reshape((3,2)).astype(int), (colors*15).astype(np.uint8))
+            # # image = cv2.fillPoly(image, (triangle.astype(np.int),), (255,255,255))
+   
     return image
 
 def rotate_triangles(triangles, rotation_matrix, center_of_rotation):
@@ -173,20 +184,26 @@ def main():
     quad_t = translate_triangles(rotate_triangles(quad.copy(), R.from_euler("XYZ", (-m.pi/2,0,0)).as_matrix(), np.array([[0,0,0]]).T), np.array([[0,0,1]]).T)
     quad_d = translate_triangles(rotate_triangles(quad.copy(), R.from_euler("XYZ", (m.pi/2,0,0)).as_matrix(), np.array([[0,0,1]]).T), np.array([[0,0,-1]]).T)
 
-    cube = np.stack((quad_f, quad_b, quad_r, quad_l, quad_t, quad_d), axis=0)
+    cube = np.stack((quad_f, quad_b, quad_r, quad_l, quad_t, quad_d), axis=0).reshape((-1,3,3))
+    triangles = cube
+
+    teapot = mesh.Mesh.from_file("Utah_teapot_(solid).stl")
+    triangles = teapot.points.reshape((-1,3,3))
+
+    colors = (np.random.rand(triangles.shape[0],3)*255).astype(np.uint8)
     while True:
         start_time = time.perf_counter()
 
         image = np.zeros((HEIGHT, WIDTH, 3), dtype=np.uint8)
-
-        triangles = cube.copy()
         
-        r = R.from_euler("XYZ", (0,0,time.perf_counter()))
+        r = R.from_euler("XYZ", (0,0,time.perf_counter()/30))
         triangles = rotate_triangles(triangles, r.as_matrix(), np.array([[0.5,0.5,0.5]]).T)
         #triangles = translate_triangles(triangles, np.array([[0,m.sin(time.perf_counter()),0]]).T)
-        camera_position = np.array([[0,-5,5]]).T
+        camera_position = np.array([[0,-25,5]]).T
         camera_rotation = R.from_euler("XYZ", (-m.pi/2, 0, 0)).as_matrix()
-        image = render_triangles(image, triangles, np.array([[255,0 ,0], [255,0 ,0], [0,0,255], [0,0,255], [0,255,0], [0,255,0], [255,0,255], [255,0,255], [0,255,255], [0,255,255], [255,255,0], [255,255,0]]), camera_position, camera_rotation, np.array([[0,-5,2]]).T)
+        image = render_triangles(image, triangles, colors, camera_position, camera_rotation, np.array([[0,-5,2]]).T, global_lighting=True)
+
+        # image = render_triangles(image, triangles, np.array([[255,0 ,0], [255,0 ,0], [0,0,255], [0,0,255], [0,255,0], [0,255,0], [255,0,255], [255,0,255], [0,255,255], [0,255,255], [255,255,0], [255,255,0]]), camera_position, camera_rotation, np.array([[0,-5,2]]).T, global_lighting=True)
         # image = cv2.putText(image, f"{1/delta_time:.2f}", (20,20), cv2.FONT_HERSHEY_SIMPLEX, 1, color=(0,255,0), thickness=1, lineType=cv2.LINE_AA)
         cv2.imshow("window", image)
         
