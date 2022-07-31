58129e853bbbff3db179e53c799aaa4661898462
batuogkoc <bgunduz21@ku.edu.tr>
2022-07-31 21:21:40 +0300

    completed vectorizing preprocessing the triangles the smooth lighting is still done per triangle, so there is no difference

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import cv2 
 import numpy as np
 import time
-from  scipy.spatial.transform import Rotation as R
+from scipy.spatial.transform import Rotation as R
+from collections import deque 
 import math as m
 WIDTH = 1600
 HEIGHT = 900
 FPS = 30
+delta_ts = deque(maxlen=30)
 prev_time = time.perf_counter()
+image = np.zeros((HEIGHT, WIDTH, 3), dtype=np.uint8)
 
 def smooth_triangle_lighting(image, triangle, colors):
     # Specify (x,y) triangle verticesq
@@ -59,10 +62,54 @@ def smooth_triangle_lighting(image, triangle, colors):
     return image
 
 def render_triangles(image, triangles, triangle_colors, camera_position, camera_rotation, light_position):
-    # print(triangles.shape)
+    # vertex_1s = triangles[:,0]
+    # vertex_2s = triangles[:,1]
+    # vertex_3s = triangles[:,2]
+    # edge_1s = vertex_1s - vertex_3s
+    # edge_2s = vertex_2s - vertex_3s
+    # normals = np.cross(edge_1s, edge_2s, axis=1)
     # centers = np.sum(triangles, axis=1)/3
-    # edge1s = triangles[:,0]-triangles[:,2]
-    # print(edge1s)
+    # distances_to_camera = centers-camera_position.T
+    # cos_angles_camera = -np.sum(normals * distances_to_camera, axis=1)
+    # mask = cos_angles_camera > 0
+
+    # vertex_1s = vertex_1s[mask]
+    # vertex_2s = vertex_2s[mask]
+    # vertex_3s = vertex_3s[mask]
+
+    # normals = normals[mask]
+
+    # triangles = triangles[mask]
+    # triangle_colors = triangle_colors[mask]
+
+    # normal_lengths = np.linalg.norm(normals, axis=1)
+
+    # v1s_distance_to_light = (vertex_1s-light_position.T)
+    # v2s_distance_to_light = (vertex_2s-light_position.T)
+    # v3s_distance_to_light = (vertex_3s-light_position.T)
+    # v1s_distance_to_light_norm = np.linalg.norm(v1s_distance_to_light, axis=1)
+    # v2s_distance_to_light_norm = np.linalg.norm(v2s_distance_to_light, axis=1)
+    # v3s_distance_to_light_norm = np.linalg.norm(v3s_distance_to_light, axis=1)
+    # v1s_cos_angle = -(np.sum(normals * v1s_distance_to_light, axis=1))/(normal_lengths*v1s_distance_to_light_norm)
+    # v2s_cos_angle = -(np.sum(normals * v2s_distance_to_light, axis=1))/(normal_lengths*v2s_distance_to_light_norm)
+    # v3s_cos_angle = -(np.sum(normals * v3s_distance_to_light, axis=1))/(normal_lengths*v3s_distance_to_light_norm)
+    # v1s_brightness = np.clip(v1s_cos_angle*(1/v1s_distance_to_light_norm**2), 0, np.inf) 
+    # v2s_brightness = np.clip(v2s_cos_angle*(1/v2s_distance_to_light_norm**2), 0, np.inf) 
+    # v3s_brightness = np.clip(v3s_cos_angle*(1/v3s_distance_to_light_norm**2), 0, np.inf) 
+    
+    # # triangles = triangles[mask]
+    # triangles = (np.linalg.inv(camera_rotation) @ (triangles.reshape(-1,3).T-camera_position)).T.reshape(-1,3,3)
+    # triangles[:,:,0] /= triangles[:,:,2]
+    # triangles[:,:,1] /= triangles[:,:,2]
+    
+    # triangles = triangles[:,:,0:2]
+    # triangles *= HEIGHT//2
+    # triangles[:,:,0] += WIDTH//2
+    # triangles[:,:,1] += HEIGHT//2
+    
+    # corner_colors = np.stack(((triangle_colors.T*v1s_brightness).T, (triangle_colors.T*v2s_brightness).T, (triangle_colors.T*v3s_brightness).T), axis=1)
+    # for colors, triangle in zip(corner_colors, triangles):
+    #     smooth_triangle_lighting(image, triangle.reshape((3,2)).astype(int), (colors*15).astype(np.uint8))
     for triangle, color in zip(triangles, triangle_colors):
         vertex_1, vertex_2, vertex_3 = triangle.reshape(-1,3)
         vertex_1 = vertex_1.reshape(3,1)
@@ -76,7 +123,7 @@ def render_triangles(image, triangles, triangle_colors, camera_position, camera_
         v1_distance_to_light = (vertex_1-light_position)
         v2_distance_to_light = (vertex_2-light_position)
         v3_distance_to_light = (vertex_3-light_position)
-        
+
         v1_distance_to_light_norm = np.linalg.norm(v1_distance_to_light)
         v2_distance_to_light_norm = np.linalg.norm(v2_distance_to_light)
         v3_distance_to_light_norm = np.linalg.norm(v3_distance_to_light)
@@ -87,7 +134,6 @@ def render_triangles(image, triangles, triangle_colors, camera_position, camera_
         v1_brightness = max(0, cos_angle_v1*(1/v1_distance_to_light_norm**2)) 
         v2_brightness = max(0, cos_angle_v2*(1/v2_distance_to_light_norm**2)) 
         v3_brightness = max(0, cos_angle_v3*(1/v3_distance_to_light_norm**2)) 
-        
         distance_to_camera = (center-camera_position)
         cos_angle_camera = -(normal.T @ distance_to_camera)
         triangle.reshape(-1,3,3)
@@ -102,7 +148,8 @@ def render_triangles(image, triangles, triangle_colors, camera_position, camera_
             triangle[:,:,1] += HEIGHT//2
 
             colors = np.array([color*v1_brightness, color*v2_brightness, color*v3_brightness])
-            image = smooth_triangle_lighting(image, triangle.reshape((3,2)).astype(int), (colors*15).astype(np.uint8))
+
+            smooth_triangle_lighting(image, triangle.reshape((3,2)).astype(int), (colors*15).astype(np.uint8))
             # image = cv2.fillPoly(image, (triangle.astype(np.int),), (255,255,255))
     return image
 
@@ -118,20 +165,22 @@ def translate_triangles(triangles, translation_vector):
 
 def main():
     global prev_time
+    quad = np.array([[[0, 0, 1], [0, 0, 0], [1, 0, 0]], [[0,0,1],[1,0,0],[1,0,1]]], dtype=np.float32)
+    quad_f = quad.copy()
+    quad_b = translate_triangles(rotate_triangles(quad.copy(), R.from_euler("XYZ", (0,0,m.pi)).as_matrix(), np.array([[0.5,0,0]]).T), np.array([[0,1,0]]).T)
+    quad_r = translate_triangles(rotate_triangles(quad.copy(), R.from_euler("XYZ", (0,0,m.pi/2)).as_matrix(), np.array([[0,0,0]]).T), np.array([[1,0,0]]).T)
+    quad_l = translate_triangles(rotate_triangles(quad.copy(), R.from_euler("XYZ", (0,0,-m.pi/2)).as_matrix(), np.array([[1,0,0]]).T), np.array([[-1,0,0]]).T)
+    quad_t = translate_triangles(rotate_triangles(quad.copy(), R.from_euler("XYZ", (-m.pi/2,0,0)).as_matrix(), np.array([[0,0,0]]).T), np.array([[0,0,1]]).T)
+    quad_d = translate_triangles(rotate_triangles(quad.copy(), R.from_euler("XYZ", (m.pi/2,0,0)).as_matrix(), np.array([[0,0,1]]).T), np.array([[0,0,-1]]).T)
+
+    cube = np.stack((quad_f, quad_b, quad_r, quad_l, quad_t, quad_d), axis=0)
     while True:
         start_time = time.perf_counter()
 
         image = np.zeros((HEIGHT, WIDTH, 3), dtype=np.uint8)
 
-        quad = np.array([[[0, 0, 1], [0, 0, 0], [1, 0, 0]], [[0,0,1],[1,0,0],[1,0,1]]], dtype=np.float32)
-        quad_f = quad.copy()
-        quad_b = translate_triangles(rotate_triangles(quad.copy(), R.from_euler("XYZ", (0,0,m.pi)).as_matrix(), np.array([[0.5,0,0]]).T), np.array([[0,1,0]]).T)
-        quad_r = translate_triangles(rotate_triangles(quad.copy(), R.from_euler("XYZ", (0,0,m.pi/2)).as_matrix(), np.array([[0,0,0]]).T), np.array([[1,0,0]]).T)
-        quad_l = translate_triangles(rotate_triangles(quad.copy(), R.from_euler("XYZ", (0,0,-m.pi/2)).as_matrix(), np.array([[1,0,0]]).T), np.array([[-1,0,0]]).T)
-        quad_t = translate_triangles(rotate_triangles(quad.copy(), R.from_euler("XYZ", (-m.pi/2,0,0)).as_matrix(), np.array([[0,0,0]]).T), np.array([[0,0,1]]).T)
-        quad_d = translate_triangles(rotate_triangles(quad.copy(), R.from_euler("XYZ", (m.pi/2,0,0)).as_matrix(), np.array([[0,0,1]]).T), np.array([[0,0,-1]]).T)
-
-        triangles = np.stack((quad_f, quad_b, quad_r, quad_l, quad_t, quad_d), axis=0)
+        triangles = cube.copy()
+        
         r = R.from_euler("XYZ", (0,0,time.perf_counter()))
         triangles = rotate_triangles(triangles, r.as_matrix(), np.array([[0.5,0.5,0.5]]).T)
         #triangles = translate_triangles(triangles, np.array([[0,m.sin(time.perf_counter()),0]]).T)
@@ -142,7 +191,8 @@ def main():
         cv2.imshow("window", image)
         
         delta_time = time.perf_counter() - start_time
-        print(f"{1/delta_time:.2f}")
+        delta_ts.append(delta_time)
+        print(f"{1/(sum(delta_ts)/len(delta_ts)):.2f}")
         delay = int(max(1, (1/FPS-delta_time)*1000))
         inp = cv2.waitKey(delay)
         if inp == ord("q"):
